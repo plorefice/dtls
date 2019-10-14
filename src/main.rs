af8fceb77979640ce3e4175fb06279cd5f55c1cc
Pietro Lorefice <pietro.lorefice@gmail.com>
2019-10-15 00:10:50 +0200

    dtls: read from either stdin or file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod ast;
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("{:#?}", ast::parse_file(std::env::args().nth(1).unwrap()));
+    let mut reader = if let Some(fname) = env::args().nth(1) {
+        Box::new(File::open(fname).unwrap()) as Box<Read>
+    } else {
+        Box::new(std::io::stdin()) as Box<Read>
+    };
+
+    println!("{:#?}", ast::parse(&mut reader));
 }
